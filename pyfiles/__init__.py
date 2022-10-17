150c720ac10e3ee5f266a4d0c3dc6d91f8478d01
konradsic <sicinskikonrad@gmail.com>
2022-10-17 09:25:55 +0200

    Add empty files with license

---
+++ pyfiles/__init__.py
@@ -0,0 +1,18 @@
+"""
+PyFiles - Python file management and downloading
+=======================
+A module that allows users to easily create, manipulate and download files from the Internet
+
+:copyright: 2022-present konradsic 
+:license: Licensed under the MIT License see LICENSE file https://github.com/konradsic/pyfiles/blob/master/LICENSE
+"""
+
+__title__ = "py-files"
+__author__ = "konradsic"
+__version__ = "0.0.1"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2022-present konradsic"
+
+from .abc import *
+from .managers import *
+from .utils import *
